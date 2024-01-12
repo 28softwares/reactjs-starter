cc74a23073df1631df4a198f42bd0bc86b55d5e5
Saroj Dahal <isarojdahal@gmail.com>
2024-01-12 22:46:31 +0545

    configured shadcn toaster and added: success variant for toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./assets/css/global.css";
+import { Toaster } from "@shadcn/ui/toaster.tsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <Toaster />
   </QueryClientProvider>
 );
